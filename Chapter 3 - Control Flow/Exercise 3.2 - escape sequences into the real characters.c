ea32978341ef854602e1c1321636f795e48bd5d7
Verkiya <himanshuverkiya@gmail.com>
2018-08-24 22:48:42 +0530

    Adding Chapter 3 solutions

---
+++ Chapter 3 - Control Flow/Exercise 3.2 - escape sequences into the real characters.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#define MAXLINE 1000
+int mgetline(char line[],int maxline);
+void escape(char s[],char t[]);
+
+
+int main(void)
+{
+    char s[MAXLINE],t[MAXLINE];
+
+    mgetline(t,MAXLINE);
+
+    escape(s,t);
+
+    printf("%s",s);
+
+    return 0;
+}
+
+void escape(char s[],char t[])
+{
+    int i,j;
+
+    i=j=0;
+
+    while(t[i] != '\0')
+    {
+        switch(t[i])
+        {
+            case '\t':
+                    s[j]='\\';
+                    ++j;
+                    s[j]='t';
+                    break;
+            case '\n':
+                    s[j]='\\';
+                    ++j;
+                    s[j]='n';
+                    break;
+            default:
+                    s[j]=t[i];
+                    break;
+        }
+        ++i;
+        ++j;
+    }
+
+    s[j]='\0';
+}
+
+int mgetline(char s[],int lim)
+{
+    int i,c;
+
+    for(i=0;i<lim-1 && (c=getchar())!=EOF;++i)
+        s[i]=c;
+
+    s[i]='\0';
+}
---
+++ Chapter 3 - Control Flow/Exercise 3.4 - itoa to handle largest negative integer.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<string.h>
+#define MAXLINE 1000
+
+#define abs(x) ( (x) > 0 ? (x): -(x))
+
+void itoa(int n,char s[]);
+void reverse(char s[]);
+
+
+int main(void)
+{
+    int number;
+    char str[MAXLINE];
+
+ /* number=-2345645; */
+
+    number = -2147483648;
+
+
+    printf("Integer %d printed as\n String:",number);
+
+    itoa(number,str);
+
+    printf("%s",str);
+
+    return 0;
+}
+
+void itoa(int n,char s[])
+{
+    int i,sign;
+
+    sign=n;
+
+    i = 0;
+
+    do
+    {
+        s[i++]= abs(n%10) + '0';
+
+    }while((n/=10)!=0);
+
+    if( sign < 0)
+        s[i++]='-';
+
+    s[i]='\0';
+
+    reverse(s);
+}
+
+void reverse(char s[])
+{
+    int c,i,j;
+
+    for(i=0,j=strlen(s)-1;i<j;i++,j--)
+        c=s[i],s[i]=s[j],s[j]=c;
+}
