45240c71eb6c997fb0a0d957151ea1538e81492e
Himanshu Verkiya <himanshuverkiya@gmail.com>
2018-08-24 23:40:43 +0530

    Create Exercise 2.5 - return the first location in the string s1 comparing s2.c

---
+++ Chapter 2 - Types- Operators and Expressions/Exercise 2.5 - return the first location in the string s1 comparing s2.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#define MAXLINE 1000
+
+int mgetline(char line[],int maxline);
+int any(char s1[],char s2[]);
+
+int main(void)
+{
+    char s1[MAXLINE],s2[MAXLINE];
+    int val;
+    
+    /* Give the first string s1 */
+    
+    mgetline(s1,MAXLINE);
+    
+    /* Give the second string s2 */
+
+    mgetline(s2,MAXLINE);
+
+    val = any(s1,s2);
+
+    printf("%d",val);
+
+    return 0;
+}
+
+int mgetline(char s[],int lim)
+{
+    int i,c;
+    for(i=0;i<lim-1 && (c=getchar())!=EOF && c!='\n';++i)
+        s[i]=c;
+
+    if(c=='\n')
+        s[i++]=c;
+    s[i]='\0';
+}
+
+int any(char s1[],char s2[])
+{
+    int i,j,check_next_char;
+
+    check_next_char=1;
+
+    for(i=0;s1[i]!='\0'&& (check_next_char);) 
+    {
+        // iterate through s2 while trying to find matching character from s1
+        for(j=0;s2[j]!='\0'&& (s1[i]!=s2[j]);++j) 
+            ; // continue
+
+        if(s2[j]=='\0') {
+            check_next_char=1;
+            i++; // go for the next char in s1
+        }
+        else
+            check_next_char=0; // match found
+    }
+
+    if(s1[i]=='\0')
+        return -1;
+    else
+        return i;
+}
