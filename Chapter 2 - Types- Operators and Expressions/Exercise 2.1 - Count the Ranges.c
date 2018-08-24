757e28857317fde14e2ae8efaf0931f9e37df6ee
Himanshu Verkiya <himanshuverkiya@gmail.com>
2018-08-24 23:39:43 +0530

    Create Exercise 2.1 - Count the Ranges.c

---
+++ Chapter 2 - Types- Operators and Expressions/Exercise 2.1 - Count the Ranges.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf("Minimum Signed Char %d\n",-(char)((unsigned char) ~0 >> 1) - 1);
+    printf("Maximum Signed Char %d\n",(char) ((unsigned char) ~0 >> 1));
+
+    printf("Minimum Signed Short %d\n",-(short)((unsigned short)~0 >>1) -1);
+    printf("Maximum Signed Short %d\n",(short)((unsigned short)~0 >> 1));
+
+    printf("Minimum Signed Int %d\n",-(int)((unsigned int)~0 >> 1) -1);
+    printf("Maximum Signed Int %d\n",(int)((unsigned int)~0 >> 1));
+
+    printf("Minimum Signed Long %ld\n",-(long)((unsigned long)~0 >>1) -1);
+    printf("Maximum signed Long %ld\n",(long)((unsigned long)~0 >> 1));
+
+    /* Unsigned Maximum Values */
+
+    printf("Maximum Unsigned Char %d\n",(unsigned char)~0);
+    printf("Maximum Unsigned Short %d\n",(unsigned short)~0);
+    printf("Maximum Unsigned Int %u\n",(unsigned int)~0);
+    printf("Maximum Unsigned Long %lu\n",(unsigned long)~0);
+
+    return 0;
+}
