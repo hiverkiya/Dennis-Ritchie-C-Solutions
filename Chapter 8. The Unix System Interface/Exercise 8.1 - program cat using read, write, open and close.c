d52fdd13e49f1baad41e703570da00d5804332d6
Himanshu Verkiya <himanshuverkiya@gmail.com>
2018-08-24 23:28:15 +0530

    Create Exercise 8.1 - program cat using read, write, open and close.c

---
+++ Chapter 8. The Unix System Interface/Exercise 8.1 - program cat using read, write, open and close.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<stdarg.h>
+#include<stdlib.h>
+
+void error(char *fmt,...);
+void filecopy(int ,int );
+
+#define STDIN 0
+#define STDOUT 1
+#define STDERR 2
+
+/* cat: concatenate files - read/write/open/close */
+
+int main(int argc,char *argv[])
+{
+    int fd;
+    
+    if(argc == 1)
+        filecopy(STDIN, STDOUT);
+    else
+        while(--argc > 0)
+            if((fd = open(*++argv,O_RDONLY)) == -1)
+                error("cat:can't open %s",*argv);
+            else
+            {
+                filecopy(fd, STDOUT);
+                close(fd);
+            }
+    return 0;
+}
+
+/* filecopy: copy file ifd to ofd */
+void filecopy(int ifd,int ofd)
+{
+    int n;
+    char buf[BUFSIZ];
+    
+    while((n=read(ifd,buf,BUFSIZ)) > 0)
+        if(write(ofd,buf,n) != n)
+            error("cat:write error");
+}
+
+
+/* error: print an error message and die */
+void error(char *fmt,...)
+{
+    va_list args;
+    
+    va_start(args,fmt);
+    fprintf((FILE *) STDERR,"error: ");
+    vfprintf((FILE *) STDERR, fmt, args);
+    fprintf((FILE *) STDERR, "\n");
+    va_end(args);
+    
+    exit(1);
+}
