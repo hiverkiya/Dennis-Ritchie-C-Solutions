69ddaedfebc61e74ef07d779d9dfe30918f9e47c
Himanshu Verkiya <himanshuverkiya@gmail.com>
2018-08-24 23:41:11 +0530

    Create Exercise 2.7-Setting bits at a position n Inverted.c

---
+++ Chapter 2 - Types- Operators and Expressions/Exercise 2.7-Setting bits at a position n Inverted.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+unsigned invert(unsigned x,int p,int n);
+
+int main(void)
+{
+    printf("%u",(unsigned)invert((unsigned)8,(int)3,(int)3));
+}
+
+unsigned invert(unsigned x, int p, int n)
+{
+     return   x ^ (~(~0 << n) << (p + 1 - n));
+}
