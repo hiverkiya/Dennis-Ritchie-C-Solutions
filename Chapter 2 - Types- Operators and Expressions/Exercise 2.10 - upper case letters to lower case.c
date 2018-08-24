0d89a602f6836319f2ea58e436c018c52abe61a6
Himanshu Verkiya <himanshuverkiya@gmail.com>
2018-08-24 23:41:56 +0530

    Create Exercise 2.10 - upper case letters to lower case.c

---
+++ Chapter 2 - Types- Operators and Expressions/Exercise 2.10 - upper case letters to lower case.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int lower(int c);
+
+int main(void)
+{
+    int c;
+    
+    while((c=getchar())!=EOF)
+    {
+        putchar(lower(c));
+    }
+}
+
+int lower(int c)
+{
+    return c>='A' && c<='Z'? c+'a'-'A':c;
+}
