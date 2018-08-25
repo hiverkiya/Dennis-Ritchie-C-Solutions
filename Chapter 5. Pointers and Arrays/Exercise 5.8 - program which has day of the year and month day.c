f77ac527388633d4d1a9551b80a74d350a484733
Himanshu Verkiya <himanshuverkiya@gmail.com>
2018-08-25 15:28:17 +0530

    Create Exercise 5.8 - program which has day of the year and month day.c

---
+++ Chapter 5. Pointers and Arrays/Exercise 5.8 - program which has day of the year and month day.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+static char daytab[2][13]={
+    {0,31,28,31,30,31,30,31,31,30,31,30,31},
+    {0,31,29,31,30,31,30,31,31,30,31,30,31}
+};
+
+int day_of_year(int year,int month,int day);
+void month_day(int year,int yearday);
+
+int main(void)
+{
+    int day,mo,dat;
+
+    day=day_of_year(1988,2,29);
+    printf("%d\n",day);
+    month_day(1988,60);
+    return 0;
+}
+
+/* day_of_year: set day of year from month & day */
+
+int day_of_year(int year,int month,int day)
+{
+    int i,leap;
+
+    leap = year % 4 == 0 && year%100 != 0 || year%400 == 0;
+    
+    for(i=1;i<month;i++)
+        day += daytab[leap][i];
+
+    return day;
+}
+
+/* month_day: set month,day from day of year */
+
+void month_day(int year,int yearday)
+{
+    int i,leap;
+
+    leap = year % 4 == 0 && year%100 != 0 || year%400 == 0;
+
+    for(i=1;yearday > daytab[leap][i];i++)
+        yearday -= daytab[leap][i];
+
+    printf("Month: %d, Day: %d\n", i, yearday);
+
+}
