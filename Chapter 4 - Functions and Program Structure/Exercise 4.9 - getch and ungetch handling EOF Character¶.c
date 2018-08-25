aad88f150318bf6d87e9eabc12fa5eb3b8171183
Himanshu Verkiya <himanshuverkiya@gmail.com>
2018-08-25 15:16:44 +0530

    Create Exercise 4.9 - getch and ungetch handling EOF Character¶.c

---
+++ Chapter 4 - Functions and Program Structure/Exercise 4.9 - getch and ungetch handling EOF Character¶.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#define BUFSIZE 100
+
+int getch(void);
+void ungetch(int c);
+
+int buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0;     /* next free position in buf */
+
+int main(void)
+{
+    int c;
+
+    c = '*';
+    
+    ungetch(c);
+
+    while((c=getch())!=EOF)
+        putchar(c);
+
+    return 0;
+}
+
+/* getch: get a (possibly pushed back) character */
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* ungetch: push a character back onto the input */
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters \n");
+    else
+        buf[bufp++] = c;
+}
