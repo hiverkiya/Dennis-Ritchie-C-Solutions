43f06b266bb7322069d90be3fc8799eecc6b2e8d
Himanshu Verkiya <himanshuverkiya@gmail.com>
2018-08-25 15:17:04 +0530

    Create Exercise 4.8 - getch and ungetch handling pushback character.c

---
+++ Chapter 4 - Functions and Program Structure/Exercise 4.8 - getch and ungetch handling pushback character.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+char buf = 0;
+
+int getch();
+void ungetch(int c);
+
+int main(void)
+{
+    int c;
+
+    c = '*';
+
+    ungetch(c);
+    
+    while((c=getch())!=EOF)
+        putchar(c);
+
+    return 0;
+}
+
+/* getch: get a (possibly) pushed back character */
+
+int getch(void)
+{
+    int c;
+
+    if(buf!=0)
+        c = buf;
+    else
+        c = getchar();
+
+    buf =0; 
+    return c;
+}
+
+/* ungetch: push a character back into input */
+
+void ungetch(int c)
+{
+    if(buf != 0)
+        printf("ungetch: too many characters\n");
+    else
+        buf = c;
+}
