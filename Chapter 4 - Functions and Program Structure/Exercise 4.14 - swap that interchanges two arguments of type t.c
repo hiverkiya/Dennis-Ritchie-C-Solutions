eab26a8cb626eb5572b6b42d5736c16454bf06e4
Himanshu Verkiya <himanshuverkiya@gmail.com>
2018-08-25 15:15:11 +0530

    Create Exercise 4.14 - swap that interchanges two arguments of type t.c

---
+++ Chapter 4 - Functions and Program Structure/Exercise 4.14 - swap that interchanges two arguments of type t.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+#define swap(t,x,y) { t _z; \
+             _z = x;\
+              x = y;\
+              y = _z; }
+
+int main(void)
+{
+    char x,y;
+    x='a';
+    y='b';
+    printf("x= %c \t y= %c\n",x,y);
+    swap(char,x,y);
+    printf("x=%c \t y=%c\n",x,y);
+}
