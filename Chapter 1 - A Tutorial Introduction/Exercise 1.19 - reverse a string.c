2ed004fdfce68b3eed476427003aa90fb8e0f220
Himanshu Verkiya <himanshuverkiya@gmail.com>
2018-08-27 09:04:27 +0530

    Update Exercise 1.19 - reverse a string.c

--- Chapter 1 - A Tutorial Introduction/Exercise 1.19 - reverse a string.c
+++ Chapter 1 - A Tutorial Introduction/Exercise 1.19 - reverse a string.c
@@ -17,3 +17,43 @@ int main(void)
 
   return 0;
 }
+
+int mgetline(char s[],int lim)
+{
+  int i,c;
+  
+  for(i=0;i<lim-1 &&(c=getchar())!=EOF && c != '\n';++i)
+    s[i] = c;
+
+  if( c == '\n')
+  {
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';
+
+  return i;
+}
+
+void reverse(char rline[])
+{
+  int i,j;
+  char temp;
+
+  for(i=0;rline[i]!='\0';++i)
+    ;
+  --i;
+  if(rline[i]=='\n')
+  --i;
+
+  j = 0;
+
+  while(j < i)
+  {
+    temp = rline[j];
+    rline[j] = rline[i];
+    rline[i] = temp;
+    --i;
+    ++j;
+  }
+}
