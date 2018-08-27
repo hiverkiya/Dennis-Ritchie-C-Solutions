d2f1e0d18ffab7b61efbcdf084e586410823cc9d
Himanshu Verkiya <himanshuverkiya@gmail.com>
2018-08-27 09:02:24 +0530

    Create Exercise 1.17 - Print lines that are longer than 80 chars.c

---
+++ Chapter 1 - A Tutorial Introduction/Exercise 1.17 - Print lines that are longer than 80 chars.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#define MAXLINE 1000
+#define LIMIT 80
+
+/* we call it ngetline, for new getline so that it does not conflict with system function getline */
+int ngetline(char line[],int lim);
+
+int main(void)
+{
+  int len;
+  char line[MAXLINE];
+
+  while((len=ngetline(line,MAXLINE)) > 0)
+  {
+    if( len > LIMIT )
+      printf("%s",line);
+  }
+
+  return 0;
+}
+
+int ngetline(char s[],int lim)
+{
+  int i,c;
+  
+  for(i=0;i<lim-1 &&(c=getchar())!=EOF && c!='\n';++i)
+    s[i]=c;
+
+  if(c == '\n')
+  {
+    s[i] = c;
+    ++i;
+  }
+    s[i] = '\0';
+
+  return i;
+}
+  
