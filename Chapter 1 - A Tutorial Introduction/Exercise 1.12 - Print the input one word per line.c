82c992a27319b4204d6913302d4da72921f9a1d9
Himanshu Verkiya <himanshuverkiya@gmail.com>
2018-08-27 09:00:41 +0530

    Create Exercise 1.12 - Print the input one word per line.c

---
+++ Chapter 1 - A Tutorial Introduction/Exercise 1.12 - Print the input one word per line.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int c;
+    c = getchar();
+    while (c != EOF) {
+        if (c == ' ') {
+            putchar('\n');
+        }
+        else {
+            putchar(c);
+        }
+        c = getchar();
+    }
+}
